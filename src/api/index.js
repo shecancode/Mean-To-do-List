dd6496c58d4142e02a6868555894785f8f2b3551
Crystal Meneses <crystalm927@gmail.com>
2018-06-13 00:00:44 -0400

    added todo route

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const express = require('express');
-
 const router = express.Router();
+const todos = require('../../mock/todos.json');
 
 
 router.get('/todos', function(req, res, next){
-  res.json({todos: []});
+  res.json({todos: todos});
 })
 
 //TODO: Add Post route to create new entries
